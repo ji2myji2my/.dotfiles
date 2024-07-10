0c7365b2eff25307077356795d71ec0545049ac4
Jimmy Mennock <jmennock@3B5.42angouleme.fr>
2024-07-10 11:03:16 +0200

    School session

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+###############################################################################
+# This is a installation script
+
+# dotfiles directory
+dotfiledir="${HOME}/dotfiles"
+
+# list of files/folders to symlink in ${homedir}
+files=(zshrc zprofile zprompt bashrc bash_profile bash_prompt aliases private)
